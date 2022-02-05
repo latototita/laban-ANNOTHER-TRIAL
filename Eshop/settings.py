d79b01a67f9a6c668b7818916ec591dc0f3d4eb6
latototita <85679433+latototita@users.noreply.github.com>
2022-02-05 17:47:36 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -15,16 +15,16 @@ import django_heroku
 import dj_database_url
 import dotenv
 
-# This is new:
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
 
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# This is new:
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
