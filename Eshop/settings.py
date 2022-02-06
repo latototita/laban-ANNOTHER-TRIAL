d1682d031e35258d91ce4dcf84788930913f9105
latototita <85679433+latototita@users.noreply.github.com>
2022-02-06 14:27:54 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -92,12 +92,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'CONN_MAX_AGE': 500
     }
 }
 
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
