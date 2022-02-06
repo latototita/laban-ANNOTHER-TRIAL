680eeb38646f49bb9d6dd8372b621174fb8e3376
latototita <85679433+latototita@users.noreply.github.com>
2022-02-06 16:58:22 -0500

    Update urls.py

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path  , include
 from django.conf.urls.static import static
 from . import settings
+from django.conf.urls import url
+from django.views.static import serve
 from django.conf.urls import (
                                 handler400,
                                 handler403,
@@ -12,7 +14,8 @@ from django.conf.urls import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-
+   url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+   url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
