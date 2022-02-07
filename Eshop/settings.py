4a24608267e2a0f96eb9e2e0125aa850e61372f4
latototita <85679433+latototita@users.noreply.github.com>
2022-02-07 11:25:36 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -155,7 +155,7 @@ STATICFILES_DIRS = (
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 #STMP configuration
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
