9e69f491e166137c4ba8907c3e47645a2997451c
latototita <85679433+latototita@users.noreply.github.com>
2022-02-07 16:40:39 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [‘127.0.0.1’,'https://pearlmartt.herokuapp.com','localhost']
+ALLOWED_HOSTS = [‘’,'https://pearlmartt.herokuapp.com','localhost']
 
 
 # Application definition
