89b5d3371aa58158ead426bf8f81de7b72ad8838
latototita <85679433+latototita@users.noreply.github.com>
2022-02-08 08:08:10 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -146,8 +146,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
