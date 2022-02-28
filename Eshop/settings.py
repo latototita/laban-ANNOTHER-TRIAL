070655b637a3e2898d4edd32c4fcd023dc1d751b
latototita <85679433+latototita@users.noreply.github.com>
2022-02-28 16:06:45 +0300

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -148,18 +148,16 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = "/image/download/"
-MEDIA_ROOT = BASE_DIR
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #STMP configuration
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
