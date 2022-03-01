051548ea1a77cf580a3d96c35857e5dad9f7a61e
latototita <85679433+latototita@users.noreply.github.com>
2022-03-01 03:10:29 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['pearlmartt.herokuapp.com', 'localhost', '127.0.0.1']
 
