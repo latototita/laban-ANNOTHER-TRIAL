7bd5792f524d64618eb8d24d9ac41e685f55258b
latototita <85679433+latototita@users.noreply.github.com>
2022-03-01 03:31:46 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -164,6 +164,6 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.mail.yahoo.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='pearlmartbusinesses@gmail.com'
+EMAIL_HOST_USER='pearlmartbusinesses@yahoo.com'
 EMAIL_HOST_PASSWORD='1234$Victoria'
 django_heroku.settings(locals())
