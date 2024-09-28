36962af58e7434de7ba832c4bd4fa0fac385d011
latototita <85679433+latototita@users.noreply.github.com>
2024-09-28 22:11:26 +0300

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -81,6 +81,7 @@ WSGI_APPLICATION = 'Eshop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+'''
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -91,7 +92,15 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+'''
+# settings.py
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Database engine
+        'NAME': BASE_DIR / 'db.sqlite3',        # Path to the database file
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
