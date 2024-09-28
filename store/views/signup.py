6ab4f4c6a37cd8c4f0424e77bf454ab67c77f7b3
latototita <85679433+latototita@users.noreply.github.com>
2024-09-28 20:35:34 +0300

    Update signup.py

--- store/views/signup.py
+++ store/views/signup.py
@@ -1,18 +1,16 @@
-from django.shortcuts import render, redirect,response
+from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
 from .home import store
 from django.views import View
 from .forms import RegistrationForm
 
-
-
-def signup(response):
-    if response.method=="POST":
-        form=RegistrationForm(response.POST)
+def signup(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('store')
+            return redirect('store')  # Redirect after successful form submission
     else:
-        form=RegistrationForm()
+        form = RegistrationForm()
 
-    return render(response,'signup.html',{"form":form})
+    return render(request, 'signup.html', {"form": form})
